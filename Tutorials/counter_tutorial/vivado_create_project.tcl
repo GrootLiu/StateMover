ec48eb05c92068469839eedc77b4823632382f6a
groot <1219671600@qq.com>
2023-10-11 06:08:25 +0000

    修改两个文件 1. 将 IPs/IL/il_vio_2.xci 文件的 IP 版本更新成版本的 IP 2. 修改 Tutorials/counter_tutorial/vivado_create_project.tcl    文件    1. 将之前加的注释除其中的一行外全部加回来    2. 修改引入文件的方法，将 add_files 改为 import_files

--- Tutorials/counter_tutorial/vivado_create_project.tcl
+++ Tutorials/counter_tutorial/vivado_create_project.tcl
@@ -20,25 +20,26 @@ create_project -part xczu2eg-sfva625-1-e -f $projName $outputDir
 # Set project properties
 set obj [current_project]
 set_property -name "board_part" -value "Interwiser:none:part0:2.0" -objects $obj
-# set_property -name "default_lib" -value "xil_defaultlib" -objects $obj
-# set_property -name "dsa.accelerator_binary_content" -value "bitstream" -objects $obj
-# set_property -name "dsa.accelerator_binary_format" -value "xclbin2" -objects $obj
-# set_property -name "dsa.board_id" -value "xczu2eg" -objects $obj
-# set_property -name "dsa.description" -value "Vivado generated DSA" -objects $obj
-# set_property -name "dsa.dr_bd_base_address" -value "0" -objects $obj
-# set_property -name "dsa.emu_dir" -value "emu" -objects $obj
-# set_property -name "dsa.flash_interface_type" -value "bpix16" -objects $obj
-# set_property -name "dsa.flash_offset_address" -value "0" -objects $obj
-# set_property -name "dsa.flash_size" -value "1024" -objects $obj
-# set_property -name "dsa.host_architecture" -value "x86_64" -objects $obj
-# set_property -name "dsa.host_interface" -value "pcie" -objects $obj
-# set_property -name "dsa.num_compute_units" -value "96" -objects $obj
-# set_property -name "dsa.platform_state" -value "pre_synth" -objects $obj
-# set_property -name "dsa.vendor" -value "xilinx" -objects $obj
-# set_property -name "dsa.version" -value "0.0" -objects $obj
+set_property -name "default_lib" -value "xil_defaultlib" -objects $obj
+set_property -name "platform.accelerator_binary_content" -value "bitstream" -objects $obj
+set_property -name "platform.accelerator_binary_format" -value "xclbin2" -objects $obj
+set_property -name "platform.board_id" -value "xczu2eg" -objects $obj
+set_property -name "platform.description" -value "Vivado generated DSA" -objects $obj
+set_property -name "platform.dr_bd_base_address" -value "0" -objects $obj
+# set_property -name "platform.emu_dir" -value "emu" -objects $obj
+set_property -name "platform.flash_interface_type" -value "bpix16" -objects $obj
+set_property -name "platform.flash_offset_address" -value "0" -objects $obj
+set_property -name "platform.flash_size" -value "1024" -objects $obj
+set_property -name "platform.host_architecture" -value "x86_64" -objects $obj
+set_property -name "platform.host_interface" -value "pcie" -objects $obj
+set_property -name "platform.num_compute_units" -value "96" -objects $obj
+set_property -name "platform.platform_state" -value "pre_synth" -objects $obj
+set_property -name "platform.vendor" -value "xilinx" -objects $obj
+set_property -name "platform.version" -value "0.0" -objects $obj
 #
 # setup design sources and constraints
-add_files [ glob top.v count.v]
+# add_files [ glob top.v count.v]
+import_files [glob top.v count.v]
 add_files -fileset constrs_1 LED_Count.xdc
 
 add_files [ glob $StateMoverPath/IPs/IL/interruption_logic_v1.v ]
